942fb71096b32004196cdeb141ae66cbed573225
Rafael Palomino <rafa14palomino@gmail.com>
2020-09-17 13:10:33 +0200

    Colores definidos

--- holamundo.java
+++ holamundo.java
@@ -4,6 +4,8 @@
 
 public class HolaMundo{
 	public static void main(String[] args) {
+		String nombre="rafa";
+		String apellido="Palomino";
 		System.out.println("Hola mundo");
 		System.out.println("Hola mundo mundillo");
 
